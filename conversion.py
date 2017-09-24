1b290dace23d50b4786559c5a82c62c49240a5aa
Sharanya9669 <sharhegde1996@gmail.com>
2017-09-24 15:27:03 +0000

    Create conversion.py

---
+++ conversion.py
@@ -0,0 +1,11 @@
+# Python Program - Convert Fahrenheit to Celsius
+
+while True:
+print("Enter 'x' for exit.")
+fah = input("Enter Temperature in Fahrenheit: ")
+if fah == 'x':
+break
+else:
+fahrenheit = float(fah)
+celsius = (fahrenheit-32)/1.8
+print("Temperature in Celsius =",celsius,"\n")
